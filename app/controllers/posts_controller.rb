722f3bd7c1765b9d44094d2c5ef807a89d6ed36f
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-04-29 17:30:05 -0400

    errors with deployment solved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: :show
   before_action :authorize_request, only: [:create, :update, :destroy]
-  before_action :set_user_post, only: [:update, :destroy]
+  before_action :set_user_post, only: [:update, :destroy, :create]
 
   # GET /posts
   def index
